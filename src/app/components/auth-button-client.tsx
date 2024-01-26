e0ffe6dd1fd4918823bb85da259ab7d78bb77363
nextokoi <nextokoi.dev@gmail.com>
2024-01-26 17:21:12 +0000

    Protected route, auth button client / server done and initial login page

---
+++ src/app/components/auth-button-client.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { type Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation'
+import { GitHubIcon } from './icons'
+
+export function AuthButton({ session } : { session: Session | null }) {
+	const supabase = createClientComponentClient()
+	const router = useRouter()
+
+	const handleSignIn = async () => {
+		await supabase.auth.signInWithOAuth({
+			provider: 'github',
+			options: {
+				redirectTo: 'http://localhost:3000/auth/callback',
+			},
+		})
+	}
+
+	const handleSignOut = async () => {
+		await supabase.auth.signOut()
+		router.refresh()
+	}
+
+	return (
+		<header>
+			{
+				session === null ? (
+					<button
+						onClick={handleSignIn}
+						type="button"
+						className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center focus:ring-gray-500 me-2 mb-2"
+					>
+						<GitHubIcon />
+						Sign in with Github
+					</button>
+					// biome-ignore lint/a11y/useButtonType: <explanation>
+				) : <button onClick={handleSignOut}>Sign Out</button>
+			}
+
+
+		</header>
+	)
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { AuthButtonServer } from '@/app/components/auth-button-server'
+import { redirect } from 'next/navigation'
+
+export default async function Home() {
+	const supabase = createServerComponentClient({ cookies })
+	const { data: { session } } = await supabase.auth.getSession()
+
+	if (session === null ){
+		redirect('/login')
+	}
+
+	const { data: activities } = await supabase.from('activities').select()
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<AuthButtonServer />
+			<pre>{JSON.stringify(activities, null, 2)}</pre>
+		</main>
+	)
+}
