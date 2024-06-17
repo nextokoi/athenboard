e375d5d65957c9246842edd26335c5c80db1a730
nextokoi <nextokoi.dev@gmail.com>
2024-06-17 19:05:22 +0100

    Update tag component

--- src/app/components/ui/tag.tsx
+++ src/app/components/ui/tag.tsx
@@ -1,18 +1,13 @@
 "use client";
-import { Tag } from "keep-react";
-import { FunctionComponent } from 'react';
+import { Tag } from "antd";
 
 type Props = {
     label: string
 }
 
-export const TagComponent: FunctionComponent<Props> = ({label}) => {
-  const handleClick = () => {
-    console.info('You clicked the Chip.');
-  }
-
+export const TagComponent = ({label} : Props) => {
   return (
-    <Tag className='bg-[#775A0B] hover:bg-[#775A0B] text-white text-base rounded-full' onClick={handleClick}>
+    <Tag color="#775A0B" className="rounded-xl px-4 py-1 cursor-pointer">
       <span className="text-sm">{label}</span>
     </Tag>
   )
