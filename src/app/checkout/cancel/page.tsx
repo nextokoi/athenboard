349616d1097f19d12a120cdcaee13a70113f7253
nextokoi <nextokoi.dev@gmail.com>
2024-06-21 13:27:00 +0100

    Redesigned success and cancel pages (Stripe)

--- src/app/checkout/cancel/page.tsx
+++ src/app/checkout/cancel/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "antd";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function page() {
-    return (
-      <div>Cancel</div>
-    )
-  }
+  return (
+    <div className="flex flex-col gap-10 justify-center items-center my-20">
+      <h1 className="text-3xl">Checkout Cancelled</h1>
+      <Image src={'https://ofrkgmwhpmpxhpjhnlro.supabase.co/storage/v1/object/public/user_images/cancel.svg'} alt="cancel" width={600} height={300} />
+      <Link href='/experiences'><Button size="large">Return Experiences</Button></Link>
+    </div>
+  )
+}
--- src/app/checkout/success/page.tsx
+++ src/app/checkout/success/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "antd";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function page() {
   return (
-    <div>Success</div>
+    <div className="flex flex-col gap-10 justify-center items-center my-20">
+      <h1 className="text-3xl">Checkout Success</h1>
+      <Image src={'https://ofrkgmwhpmpxhpjhnlro.supabase.co/storage/v1/object/public/user_images/success.svg'} alt="success" width={600} height={300} />
+      <Link href='/experiences'><Button size="large">Return Experiences</Button></Link>
+    </div>
   )
 }
