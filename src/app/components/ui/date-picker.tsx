18617b9769595982f5267d4c1a317d18a1ee842b
nextokoi <nextokoi.dev@gmail.com>
2024-06-22 17:26:29 +0100

    Remove calendar from phosphor-react and format from date-fns unused imports

--- src/app/components/ui/date-picker.tsx
+++ src/app/components/ui/date-picker.tsx
@@ -1,10 +1,6 @@
 'use client'
 
-import { format } from 'date-fns'
-import { Calendar } from 'phosphor-react'
-/* import { Button, DatePicker, Popover } from 'keep-react' */
 import { DatePicker } from 'antd'
-
 
 interface DatePickerProps {
   onDateChange: (date: Date | undefined) => void
@@ -13,20 +9,6 @@ interface DatePickerProps {
 
 export const DatePickerComponent = ({ onDateChange, date }: DatePickerProps) => {
   return (
-/*     <Popover showArrow={false} placement="bottom-start">
-      <Popover.Action asChild>
-        <Button
-          className="w-full justify-start gap-2 rounded-xl border border-metal-50 px-4 text-left text-body-4 font-normal hover:bg-white active:focus:scale-100"
-          variant="outline"
-          color="secondary">
-          <Calendar size={20} color="#AFBACA" />
-          {date ? format(date ?? new Date(), 'PPP') : <span>Select Your Date</span>}
-        </Button>
-      </Popover.Action>
-      <Popover.Content className="z-50 max-w-min">
-        <DatePicker mode="single" selected={date} onSelect={onDateChange} showOutsideDays={true} />
-      </Popover.Content>
-    </Popover> */
     <DatePicker onChange={onDateChange} value={date} className="w-full py-3"/>
   )
 }
